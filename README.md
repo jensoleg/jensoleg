7b8b55543c881527802f89d4b54d7cf1e5de43f8
Jens-Ole Graulund <jensole@graulund.net>
2013-11-14 13:40:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/jensoleg/herokuapp.png?branch=master)](https://travis-ci.org/jensoleg/herokuapp)
---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('READyApp', [
+  'ngResource', 'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
